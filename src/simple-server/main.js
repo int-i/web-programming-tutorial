8f4e2247a45b30b61cc4524be817bb4edb264caf
Seungjae Park <astro.psj@gmail.com>
2020-01-13 01:27:43 +0900

    Add comment

--- src/simple-server/main.js
+++ src/simple-server/main.js
@@ -1,29 +1,47 @@
+// 모듈 가져오기
 const polka = require('polka');
 const send = require('@polka/send-type');
+// public 폴더를 지정한다.
 const serve = require('sirv')('public');
+// JSON 파일도 읽어올 수 있다.
 const users = require('./users');
 
 const { PORT = 3000 } = process.env;
 
+// req: 서버에 들어온 정보(request)
+// res: 서버가 보내줄 정보(response)
+// See(화살표함수): https://poiemaweb.com/es6-arrow-function
 const find = (req, res) => {
+    // 사용자 아이디(객체 구조 분해 할당 문법)
+    // See: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
     const { id } = req.params;
-    for (const user of users) {
-        if (user.id === id) {
-            send(res, 200, user);
-            return;
-        }
+    // 아이디로 사용자 검색, 없으면 undefined 반환
+    // See: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    const user = users.find(user => user.id === id);
+    // Boolean(undefined) === false
+    if (user) {
+        // 200 OK 사용자 정보 전송
+        send(res, 200, user);
+    } else {
+        // 404 Not Found 전송
+        send(res, 404, null);
     }
-    send(res, 404, null);
 };
 
 const getAll = (req, res) => {
+    // 200 OK 모든 사용자 정보 전송
     send(res, 200, users);
 };
 
+// polka 객체 소환
 polka()
+    // serve 모듈은 지정한 폴더(public)의 모든 파일을 외부에서 접근 가능하게 한다.
     .use(serve)
+    // 브라우저가 GET /users로 접속했을 때 getAll을 실행한다.
     .get('/users', getAll)
+    // 브라우저가 GET /users/id로 접속했을 때 find를 실행한다.
     .get('/users/:id', find)
+    // 서버를 실행한다.
     .listen(PORT, err => {
         if (err) throw err;
         console.log(`> Running on localhost:${PORT}`);
--- src/simple-server/public/main.js
+++ src/simple-server/public/main.js
@@ -1,8 +1,15 @@
 const find = (id) => {
+    // See: https://developer.mozilla.org/ko/docs/Web/API/Fetch_API/Fetch%EC%9D%98_%EC%82%AC%EC%9A%A9%EB%B2%95,
+    //      https://joshua1988.github.io/web-development/javascript/promise-for-beginners/,
+    //      https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Promise
     fetch(`/users/${id}`).then((res) => {
+        // 응답이 정상적이면(=404등이 아니면)
         if (res.ok) {
+            // JSON 형식으로 변환
             res.json().then((user) => {
+                // 로그 입력하기
                 console.log('user:', user);
+                // 팝업창 보여주기
                 alert(`name: ${user.name}`);
             });
         } else {
@@ -24,7 +31,9 @@ const getAll = () => {
     });
 };
 
+// id="find"인 요소(Element)를 찾아서 클릭시 실행되는 함수를 붙여줌
 document.querySelector('#find').addEventListener('click', () => {
+    // id="user-id"인 요소(Element)의 값
     const input = document.querySelector('#user-id').value;
     console.log('input:', input);
     if (input === 'all') {
